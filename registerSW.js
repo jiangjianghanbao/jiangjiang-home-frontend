5e8ee3c60df3ac371ac549a5e3b0a6b7a2b35b04
yujin <yujin@coze.email>
2026-08-11 18:37:12 +0800

    Deploy: 独立APP+天气+生理期+朋友圈+日记

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/jiangjiang-home-frontend/sw.js', { scope: '/jiangjiang-home-frontend/' })})}
